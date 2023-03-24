6727b2bd80ff22045c2368263e070cdb46c8558a
Rubén J <rubenjr@protonmail.com>
2023-03-24 03:31:42 +0100

    🎉 depth perception project - poetry for management - placeholder scripts - gitignore and project structure

---
+++ depth_estimation/src/net.py
@@ -0,0 +1,57 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+
+class ConvBlock(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.conv_1 = nn.Conv2d(16, 16, kernel_size=3, padding=1)
+        self.conv_2 = nn.Conv2d(16, 16, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+
+    def forward(self, x):
+        x = F.relu(self.conv_1(x))
+        x = F.relu(self.conv_2(x))
+        return self.pool(x)
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=3, padding=1)
+        self.conv_block_1 = ConvBlock()
+        self.conv_block_2 = ConvBlock()
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(16 * 16, 768)
+        self.fc2 = nn.Linear(768, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+
+        x = self.conv_block_1(x)  # Applies 2 convolutions and 1 MaxPool
+        x = self.conv_block_2(x)  # Applies 2 convolutions and 1 MaxPool
+
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        # Softmax so the sum of probabilities is 1
+        return F.softmax(self.fc4(x), 1)
+
+
+net = Net()
+
+
+def main():
+    input = torch.rand(3, 32, 32).unsqueeze(0)
+    print(input)
+    output = net(input)
+    print(f'Output shape: {output.shape}')
+    print(f'Output: {output}')
